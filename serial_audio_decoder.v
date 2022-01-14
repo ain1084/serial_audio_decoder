ad687a01e87bbd00ffeca7e464d01950248e78fb
Seiji Ainoguchi <14823909+ain1084@users.noreply.github.com>
2022-01-15 03:39:33 +0900

    Added audio_width parameter

--- serial_audio_decoder.v
+++ serial_audio_decoder.v
@@ -1,6 +1,6 @@
 `default_nettype none
 
-module serial_audio_decoder(
+module serial_audio_decoder #(parameter audio_width = 32)(
     input wire sclk,
     input wire reset,
     input wire lrclk,
@@ -11,7 +11,7 @@ module serial_audio_decoder(
     output reg o_valid,
     input wire o_ready,
     output reg o_is_left,
-    output reg [31:0] o_audio);
+    output reg [audio_width-1:0] o_audio);
 
     reg [4:0] bit_count;
     reg [31:0] shift_data;
@@ -19,6 +19,8 @@ module serial_audio_decoder(
 
     wire is_current_channel_left = lrclk == lrclk_polarity;
     wire is_lr_changed = is_i2s ? (lr_history[0] != lr_history[1]) : (lr_history[0] != is_current_channel_left);
+
+    localparam [31:0] ZERO = 0;
 
     always @(posedge sclk or posedge reset) begin
         if (reset) begin
@@ -37,25 +39,25 @@ module serial_audio_decoder(
             if (is_lr_changed && o_is_left != lr_history[1]) begin
                 case (bit_count)
                     5'd31: begin // 32bit
-                        o_audio <= shift_data;
+                        o_audio <= shift_data[31:32 - audio_width];
                         is_error <= 1'b0;
                         o_is_left <= lr_history[1];
                         o_valid <= 1'b1;
                     end
                     5'd23: begin // 24bit
-                        o_audio <= { shift_data[23:0], 8'b0 };
+                        o_audio <= (audio_width > 24) ? { shift_data[23:0], ZERO[audio_width - 1 - 24:0] } : shift_data[23:24 - audio_width];
                         is_error <= 1'b0;
                         o_is_left <= lr_history[1];
                         o_valid <= 1'b1;
                     end
                     5'd15: begin // 16bit
-                        o_audio <= { shift_data[15:0], 16'b0 };
+                        o_audio <= (audio_width > 16) ? { shift_data[15:0], ZERO[audio_width - 1 - 16:0] } : shift_data[15:16 - audio_width];
                         is_error <= 1'b0;
                         o_is_left <= lr_history[1];
                         o_valid <= 1'b1;
                     end
                     default: begin  //error
-                        o_audio <= shift_data;
+                        o_audio <= shift_data[31:32 - audio_width];
                         is_error <= 1'b1;
                         o_is_left <= lrclk_polarity;
                         o_valid <= 1'b0;
